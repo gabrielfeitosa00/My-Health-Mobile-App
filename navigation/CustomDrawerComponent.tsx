a5b34d5f17f7e5c26994868264b0d091b3fa591f
Gabriel Perino Feitosa <50030104+gabrielfeitosa00@users.noreply.github.com>
2023-04-27 20:35:11 -0300

    feat: drawer style

--- navigation/CustomDrawerComponent.tsx
+++ navigation/CustomDrawerComponent.tsx
@@ -4,18 +4,41 @@ import {
   DrawerItemList,
 } from '@react-navigation/drawer';
 import {Text, View} from 'react-native';
+import {Divider} from 'react-native-paper';
 
 const CustomDrawer = props => {
   return (
-    <DrawerContentScrollView {...props}>
+    <DrawerContentScrollView {...props} style={{backgroundColor: '#ADD4D0'}}>
       <View>
-        <Text style={{fontSize: 12, color: 'blue', alignSelf: 'center'}}>
+        <Text
+          style={{
+            fontSize: 22,
+            color: '#419ED7',
+            alignSelf: 'center',
+            fontFamily: 'AveriaLibre-Regular',
+            marginTop: 10,
+          }}>
           Olá User
         </Text>
+        <Divider
+          style={{
+            backgroundColor: '#419ED7',
+            width: '85%',
+            alignSelf: 'center',
+            height: 2,
+            marginTop: 20,
+            marginBottom: 30,
+          }}
+          bold={true}
+        />
       </View>
       <DrawerItemList {...props} />
       <DrawerItem
-        labelStyle={{color: 'white'}}
+        labelStyle={{
+          color: '#419ED7',
+          fontSize: 22,
+          fontFamily: 'AveriaLibre-Regular',
+        }}
         label="Sair"
         onPress={() => {
           props.navigation.popToTop();
